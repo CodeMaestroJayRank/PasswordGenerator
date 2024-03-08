ed46bddfcb236b18ec5f3257bb50fe37bd7347e0
jayranktjcg <jayrank.developer@gmail.com>
2024-03-08 23:05:02 +0530

    Completed project. need to fix some bugs in currency dropdown

--- currencyconvertor/src/hooks/useCurrencyInfo.js
+++ currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency) {
-    const [data, setData] = useState({});
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        // fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`) this is old API 
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) => setData(res[currency]));
-
-        console.log("Data :: ",data);
-    }, [currency]);
-    console.log("Return Data :: ",data);
-    return data;
+        .then((res) => setData(res[currency]))
+    }, [currency])
+    return data
 }
 
 export default useCurrencyInfo;
