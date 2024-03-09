fee98ef86c5785e35b994f17f947a19bbacab1bc
jayranktjcg <jayrank.developer@gmail.com>
2024-03-09 17:10:53 +0530

    Completed react routing learning with best practices

---
+++ 02reactrouter/src/components/Contact/Contact.jsx
@@ -0,0 +1,99 @@
+export default function Contact() {
+    return (
+        <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
+            <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div className="mt-8 overflow-hidden">
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
+                            <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
+                                Get in touch:
+                            </h1>
+                            <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
+                                Fill in the form to start a conversation
+                            </p>
+
+                            <div className="flex items-center mt-8 text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                </svg>
+
+                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                                    Acme Inc, Street, State, Postal Code
+                                </div>
+                            </div>
+
+                            <div className="flex items-center mt-4 text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                                </svg>
+
+                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                                    +44 1234567890
+                                </div>
+                            </div>
+
+                            <div className="flex items-center mt-2 text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
+                                </svg>
+
+                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                                    info@acme.org
+                                </div>
+                            </div>
+                        </div>
+
+                        <form className="p-6 flex flex-col justify-center">
+                            <div className="flex flex-col">
+                                <label htmlFor="name" className="hidden">
+                                    Full Name
+                                </label>
+                                <input
+                                    type="name"
+                                    name="name"
+                                    id="name"
+                                    placeholder="Full Name"
+                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                                />
+                            </div>
+
+                            <div className="flex flex-col mt-2">
+                                <label htmlFor="email" className="hidden">
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    placeholder="Email"
+                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                                />
+                            </div>
+
+                            <div className="flex flex-col mt-2">
+                                <label htmlFor="tel" className="hidden">
+                                    Number
+                                </label>
+                                <input
+                                    type="tel"
+                                    name="tel"
+                                    id="tel"
+                                    placeholder="Telephone Number"
+                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
+                            >
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
